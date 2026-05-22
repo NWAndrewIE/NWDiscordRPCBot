918cdc53f847c964b65f498e6ecc9bb92b059665
Andrew B. <137150378+NWAndrewIE@users.noreply.github.com>
2026-05-23 00:23:01 +1000

    Add license and contact information to setup_master.py
    Added licensing information and contact details to the setup_master.py file.

--- setup_master.py
+++ setup_master.py
@@ -1,3 +1,24 @@
+"""
+NW Discord RPC Bot - a bot program for emulating Discord RPC state
+Copyright (C) 2026 Andrew Brownington
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see
+<https://www.gnu.org/licenses/>.
+
+Contact: contact@naruwhite.lol
+"""
+
 import os
 import sys
 import datetime
